f8a9cf1228090f96d894d37ccda18f347478e663
Александр Григоров <grigorovalex30@yandex.ru>
2025-02-04 20:22:08 +0300

    04.02.2025

---
+++ dostavka.py
@@ -0,0 +1,16 @@
+#id132826561
+weight_robots = sorted(list(map(int, input().split()))) 
+limit = int(input())
+i = 0 # первый элемент в списке
+j = len(weight_robots) - 1 # последний элемент в списке
+count = 0 # количество платформ
+while i<=j:
+    total_weight = weight_robots[i] + weight_robots[j]
+    if total_weight <= limit:
+        count += 1
+        i += 1
+        j -= 1
+    else:
+        count += 1
+        j -=1
+print(count)
